68c14428e96664efabc027c13dd2984b8e37c593
piyudhorey1 <piyushdhorey1@gmail.com>
2024-07-11 20:36:19 +0530

    Completed setup for project

---
+++ app/src/main/java/com/example/codeland/fragment/HomeFragment.kt
@@ -0,0 +1,12 @@
+package com.example.codeland.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.codeland.R
+
+class HomeFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_fragment)
+    }
+}
